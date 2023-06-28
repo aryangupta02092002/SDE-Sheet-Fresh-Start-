aff14e28c0c55847183dfd6159acbd0364ae6765
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-06-29 00:02:09 +0530

    Greedy | Sorting | Platforms Needed Problem

--- Day8_GreedyAlgorithm/MinimumPlatforms.cpp
+++ Day8_GreedyAlgorithm/MinimumPlatforms.cpp
@@ -18,6 +18,24 @@ return the maximum value at the end.
 TC: O(N^2)
 SC: O(1)
 */
+int countPlatforms(int n,int arr[],int dep[])
+ {
+    int ans=1;
+    for(int i=0;i<=n-1;i++)
+    {
+        int count=1; 
+        for(int j=i+1;j<=n-1;j++)
+        {
+            if((arr[i]>=arr[j] && arr[i]<=dep[j]) ||
+           (arr[j]>=arr[i] && arr[j]<=dep[i]))
+           {
+               count++;
+           }
+        }
+        ans=max(ans,count); 
+    }
+    return ans;
+ }
 
 
 //Efficient
